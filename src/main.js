3f366eeef2f5eabc7de8e1897c0b1f3f5042a871
Nicole Henríquez <ale.nicolehs@gmail.com>
2022-12-06 00:55:23 -0300

    estilos bbanner azul totoro.gif

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+//console.log(data.films[0].poster)
+
+let peliculas = data.films;
+let containerGhibli = document.getElementById("root");
+
+//peliculas.forEach( films => console.log(films) )
+// crear una constante para guardar 
+const displayCard = (dataGhibli) => {
+    dataGhibli.forEach((films) => {
+        let cardFilms = document.createElement("div");
+        cardFilms.classList.add("carta");
+        cardFilms.innerHTML += `<img src="${films.poster}" /> 
+        <section class= "infoFilms"> 
+        <h2 class="nameMovie" > ${films.title} </h2>
+        <p class= "text1"> ${films.director} </p>
+        <p class= "text"> ${films.release_date} </p>
+        <p class= "cajatext"> ${films.description} </p>
+        </section>
+        `
+        containerGhibli.appendChild(cardFilms);
+    })
+}
+
+displayCard(peliculas); // entregamos el argumento, para filtrar u ordenar le paso la data a displaycard para recorrer y mostrar
+
+console.log(peliculas);
